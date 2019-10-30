2c8361bf8e04239e1febc7ac1f93c0cd1db601b8
Student <example@example.com>
2019-10-30 14:31:58 -0700

    add else catcher for inputs that are all consonants

--- js/scripts2.js
+++ js/scripts2.js
@@ -6,14 +6,14 @@ var vowels = ["a","e","i","o","u","y","A","E","I","O","U","Y"];
 var pigSentence = function(input){
   var sentence = input.split(' ');
   newSentence = sentence.map(function(word){
-      // console.log(pigWord(word));
 
-      return pigWord(word);
+    return pigWord(word);
   });
-  // console.log(newSentence);
   return newSentence.join(' ');
 };
 
+
+//below focused on singluar word picked from the sentence array
 var pigWord = function(word) {
   if (word[0] ==='y' || word[0] === 'Y') {
     return word.slice(1) + 'yay';
@@ -22,29 +22,24 @@ var pigWord = function(word) {
   } else {
     for (i=0; i < word.length; i++){
       if (word[i] === 'u' || word[i] === 'U' && word[i-1] === 'q' || word[i-1] === 'Q') {
-        return word.slice(i+1) + word.slice(0,i+1) + 'ay';
+        return (word.slice(i+1) + word.slice(0,i+1) + 'ay');
       } else if(vowels.includes(word[i])) {
-        return(word.slice(i) + word.slice(0,i) + 'ay');
-      } else {
-        return (pigWord("Error"))
+        return (word.slice(i) + word.slice(0,i) + 'ay');
       }
-    }
+    } return 'error';
   }
 }
 
 
 
+// User Logic
+$(document).ready(function(){
+  $("#pigForm").submit(function(event){
+    var englishInput = $("#english-input").val();
 
-
-
-  // User Logic
-  $(document).ready(function(){
-    $("#pigForm").submit(function(event){
-      var englishInput = $("#english-input").val();
-
-      var result = pigSentence(englishInput);
-      console.log(result);
-      event.preventDefault();
-    });
-
+    var result = pigSentence(englishInput);
+    $('#output').text(result);
+    event.preventDefault();
   });
+
+});
